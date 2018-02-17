83750348d15f62f30b197807a1d4dc002ada1b5a
ishan1108 <patel.ish@husky.neu.edu>
2018-02-17 14:26:57 -0500

    Menu

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,26 @@
 #include "SDL2/SDL_ttf.h"
 #include "menu.h"
 
+const int SCREEN_HEIGHT = 640;
+const int SCREEN_WIDTH = 480;
+
 int main(int argc, char* argv[]){
+
+    TTF_Init();
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("Breakout",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SCREEN_WIDTH,
+                                          SCREEN_HEIGHT,
+                                          SDL_WINDOW_SHOWN);
+
+    SDL_Surface* surface = SDL_GetWindowSurface(window);
+
+    TTF_Font* font = TTF_OpenFont("SIXTY.ttf", 15);
+
+    menu mainMenu = menu();
+    mainMenu.displayMenu(surface,font);
+
     return 0;
 }
--- src/mainmenu.cpp
+++ src/mainmenu.cpp
@@ -1,9 +1,11 @@
 #include "menu.h"
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_ttf.h"
+#include "string"
 
 menu::menu()
 {
+
 }
 
 menu::~menu()
@@ -13,7 +15,26 @@ menu::~menu()
 
 int menu::displayMenu(SDL_Surface* window, TTF_Font* font){
 
-    const int OPTIONS = 4;
+    Uint32 time;
+    int x,y;
+    const int OPTIONS = 3;
+    const char* MENU_OPTIONS[OPTIONS] = {"Play","Settings","Exit"};
+    SDL_Surface* MENU_ITEMS[OPTIONS];
+    bool isSelected[OPTIONS] = {0,0,0};
+    SDL_Color color[2] = {{255,0,0},{0,255,0}};
+
+    MENU_ITEMS[0] = TTF_RenderText_Solid(font,MENU_OPTIONS[0],color[0]);
+    MENU_ITEMS[1] = TTF_RenderText_Solid(font,MENU_OPTIONS[1],color[0]);
+    MENU_ITEMS[2] = TTF_RenderText_Solid(font,MENU_OPTIONS[2],color[0]);
+
+    SDL_Rect position[OPTIONS];
+    position[0].x = 200;
+    position[0].y = 100;
+    position[1].x = 300;
+    position[1].y = 100;
+    position[2].x = 400;
+    position[2].y = 100;
+
     return 0;
 
 }
